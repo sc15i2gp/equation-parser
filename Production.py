0b2de62b86c6c42c6a884893e1e58f3210699d90
sc15i2gp <sc15i2gp@leeds.ac.uk>
2017-09-23 01:10:35 +0100

    Moved Production class to its own file.

---
+++ Production.py
@@ -0,0 +1,5 @@
+class Production:
+    # Leftside and rightSide refer to the sides of the production rules as written
+    def __init__(self, leftSide, rightSide):
+        self.leftSide = leftSide
+        self.rightSide = rightSide
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-class Production:
-    # Leftside and rightSide refer to the sides of the production rules as written
-    def __init__(self, leftSide, rightSide):
-        self.leftSide = leftSide
-        self.rightSide = rightSide
+from Production import *
 
 def main():
     p = Production("a", "b")
